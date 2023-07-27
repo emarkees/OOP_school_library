4fdda73c3739551b68414d3ac780411d4d9cf71e
emarkees <rphillipse@gmail.com>
2023-07-27 01:47:03 -0600

    Update person.rb files

--- classes/person.rb
+++ classes/person.rb
@@ -1,26 +1,31 @@
-class Person
-  def initialize(age, name = 'Unknown', parent_permission: true)
-    @id = Random.rand(1..1000)
-    @name = name
-    @age = age
-    @parent_permission = parent_permission
-  end
+# ./classes/person.rb
+require './classes/nameable'
 
-  # Gett methods
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
-  # Private method
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    super()
+    @age = age
+    @parent_permission = parent_permission
+    @name = name
+  end
+
   private
 
   def of_age?
     @age >= 18
   end
 
-  # Public method
   public
 
   def can_use_service
     of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 end
